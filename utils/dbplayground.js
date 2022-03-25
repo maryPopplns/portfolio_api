9f3530d506bea7821c4a66fee9c44607534833c2
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-25 13:46:46 -0700

    added schema files

--- utils/dbplayground.js
+++ utils/dbplayground.js
@@ -3,5 +3,3 @@ const mongoose = require('mongoose');
 const { logger } = require(path.join(__dirname, '../config/logger.js'));
 
 require(path.join(__dirname, '../config/database'));
-
-(function addUser() {})();

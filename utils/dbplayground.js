68f5049a1ce92618538757ba03e279e1f1d03a33
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-26 09:33:22 -0700

    reformatted create user function

--- utils/dbplayground.js
+++ utils/dbplayground.js
@@ -9,30 +9,64 @@ const Comment = require(path.join(__dirname, '../models/comment'));
 require(path.join(__dirname, '../config/database'));
 const closeConnection = () => mongoose.connection.close();
 
-const userName = 'spencer';
-const password = '123';
+const username = 'michael';
+const password = '1234';
 
-function createUser() {
-  bcrypt.genSalt(10, function (error, salt) {
-    if (error) {
-      return logger.error('generating salt error: ', error);
-    }
-    bcrypt.hash(password, salt, function (error, hash) {
-      if (error) {
-        return logger.error('generating hash error: ', error);
-      }
-      User.create({
-        username: userName,
-        password: hash,
-      })
-        .then((result) => logger.info(result))
-        .catch((error) => logger.error(error))
-        .finally(() => {
-          closeConnection();
-        });
+async function createUser() {
+  // generate salt
+  let salt;
+  await bcrypt
+    .genSalt(10)
+    .then((result) => (salt = result))
+    .catch((error) => {
+      closeConnection();
+      return console.log(`error generating salt: ${error}`);
     });
-  });
+
+  // hash password
+  let hashedPassword;
+  salt &&
+    (await bcrypt
+      .hash(password, salt)
+      .then((result) => (hashedPassword = result))
+      .catch((error) => {
+        closeConnection();
+        logger.error(`error generating hash ${error}`);
+      }));
+
+  // create user
+  hashedPassword &&
+    (await User.create({
+      username: username,
+      password: hashedPassword,
+    })
+      .then((result) => logger.info(result))
+      .catch((error) => logger.error(`error saving user: ${error}`))
+      .finally(() => {
+        closeConnection();
+      }));
 }
+// function createUser() {
+//   bcrypt.genSalt(10, function (error, salt) {
+//     if (error) {
+//       return logger.error('generating salt error: ', error);
+//     }
+//     bcrypt.hash(password, salt, function (error, hash) {
+//       if (error) {
+//         return logger.error('generating hash error: ', error);
+//       }
+//       User.create({
+//         username: username,
+//         password: hash,
+//       })
+//         .then((result) => logger.info(result))
+//         .catch((error) => logger.error(error))
+//         .finally(() => {
+//           closeConnection();
+//         });
+//     });
+//   });
+// }
 
 function createPost() {
   Post.create({

bd726dafe573de1549dbd7273dd95e3fb768a47e
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-25 18:56:07 -0700

    added auth dependencies. configured local and jwt strategies.

---
+++ config/auth.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const path = require('path');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require(path.join(__dirname, '../models/user'));
+
+// [ DEFINE LOCAL STRATEGY ]
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username }, (error, user) => {
+      if (error) {
+        return done(error);
+      }
+      if (!user) {
+        return done(null, false, { message: 'incorrect username' });
+      }
+      bcrypt.compare(password, user.password, (error, res) => {
+        if (res) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: 'incorrect password' });
+        }
+      });
+    });
+  })
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    function (jwtPayload, done) {
+      const id = jwtPayload.data._id;
+      User.findById(id)
+        .then((user) => {
+          if (!user) {
+            done(null, false, { message: 'no user' });
+          } else {
+            done(null, user);
+          }
+        })
+        .catch((error) => done(error));
+    }
+  )
+);

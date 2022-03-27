112467dfaf2b067b7ebce2d1e9646c19a1ece6e6
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-27 11:46:33 -0700

    created auth 401 & 403 responses for createPost

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,9 +1,16 @@
 require('dotenv').config();
 
 exports.createPost = [
-  function authentication(req, res, next) {
+  function isLoggedIn(req, res, next) {
+    req.user && next();
+    !req.user && res.status(401).json({ messsage: 'unauthorized' });
+  },
+  function isSuperUser(req, res, next) {
     const isSuper = req.user.id === process.env.SUPER_USER_ID;
     isSuper && next();
-    !isSuper && next({ status: 403, message: 'not super user' });
+    !isSuper && res.status(403).json({ message: 'forbidden' });
+  },
+  function createPost(req, res, next) {
+    res.end('this is the end');
   },
 ];

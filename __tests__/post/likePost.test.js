5aab65725f4333f27e31811860fa54be06f9279a
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-31 09:45:41 -0700

    updated deletePost

---
+++ __tests__/post/likePost.test.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const path = require('path');
+const async = require('async');
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const { logger } = require(path.join(__dirname, '../../config/logger'));
+// setups
+const { app, request } = require(path.join(__dirname, '../setup/appSetup'));
+const mongoDB = require(path.join(__dirname, '../setup/mongoSetup'));
+require(path.join(__dirname, '../../config/passport'));
+// jwt auth
+const auth = require(path.join(__dirname, '../../middleware/jwtAuth.js'));
+app.use(auth);
+// routes
+const postRoute = require(path.join(__dirname, '../../routes/postRoute'));
+const userRoute = require(path.join(__dirname, '../../routes/userRoute'));
+app.use('/post', postRoute);
+app.use('/user', userRoute);
+// user model
+const User = require(path.join(__dirname, '../../models/user'));
+const Post = require(path.join(__dirname, '../../models/post'));
+
+describe('PUT /post/like/:id', () => {
+  // initialize DB
+  mongoDB();
+
+  async function createUsers() {
+    // let salt;
+    // let hashedPassword;
+
+    // // create salt
+    // await bcrypt
+    //   .genSalt(10)
+    //   .then((result) => (salt = result))
+    //   .catch((error) => logger.error(`${error}`));
+
+    // // create hashed password
+    // await bcrypt
+    //   .hash('123', salt)
+    //   .then((result) => (hashedPassword = result))
+    //   .catch((error) => logger.error(`${error}`));
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync('123', salt);
+
+    // super user
+    await User.create({
+      username: 'spencer',
+      password: hashedPassword,
+    }).catch((error) => logger.error(`${error}`));
+  }
+
+  beforeAll(createUsers);
+
+  test.skip('user needs to be superUser', (done) => {
+    expect(1).toBe(1);
+    done();
+  });
+});
